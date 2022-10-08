8d19d15672897d3cb682824e3d9af0e5af4a7c98
teraclitos <tefans12@gmail.com>
2022-10-07 23:16:44 -0300

    agrego detalle syntaz error y fuerzo a resetear la caja registradora en caso de syntax error y math error

--- js/index.js
+++ js/index.js
@@ -18,200 +18,257 @@ const number = (number) => {
   const display = document.getElementById("display");
   arrayNumber.push(number);
   if (arrayNumber.length < 17) {
-    display.innerText = `${arrayNumber.join("")}`;
+    if (
+      display.innerText !== "MATH ERROR" &&
+      display.innerText !== "SYNTAX ERROR"
+    ) {
+      display.innerText = `${arrayNumber.join("")}`;
+    } else {
+      alert("Debe resetear la caja registardora");
+    }
   } else {
     alert("16 cifras es el límite permitido");
   }
 };
 
 const operations = (operacion) => {
-  switch (operacion) {
-    case `suma`:
-      if (substractionCondition === true) {
-        sum = mixCalc - parseFloat(arrayNumber.join(""));
-      } else if (multiplicationCondition === true) {
-        sum = mixCalc * parseFloat(arrayNumber.join(""));
-      } else if (divisionCondition === true) {
-        sum = mixCalc / parseFloat(arrayNumber.join(""));
-      } else {
-        if (sumCondition === true) {
-          sum = sum + parseFloat(arrayNumber.join(""));
-        } else {
-          (sum = parseFloat(arrayNumber.join("")))
-            ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
-            : (sum = calc);
-        }
-      }
-
-      mixCalc = sum;
-      arrayNumber = [];
-      initialValueDisplay();
-
-      substraction = 0;
-      multiplication = 0;
-      division = 0;
-      substractionCondition = false;
-      multiplicationCondition = false;
-      divisionCondition = false;
-      sumCondition = true;
-
-      break;
-    case `resta`:
-      if (sumCondition === true) {
-        substraction = mixCalc + parseFloat(arrayNumber.join(""));
-      } else if (multiplicationCondition === true) {
-        substraction = mixCalc * parseFloat(arrayNumber.join(""));
-      } else if (divisionCondition === true) {
-        substraction = mixCalc / parseFloat(arrayNumber.join(""));
-      } else {
+  if (
+    display.innerText !== "MATH ERROR" &&
+    display.innerText !== "SYNTAX ERROR"
+  ) {
+    switch (operacion) {
+      case `suma`:
         if (substractionCondition === true) {
-          substraction = substraction - parseFloat(arrayNumber.join(""));
-        } else {
-          (substraction = parseFloat(arrayNumber.join("")))
-            ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
-            : (substraction = calc);
-        }
-      }
-
-      mixCalc = substraction;
-      arrayNumber = [];
-      initialValueDisplay();
-
-      sum = 0;
-      multiplication = 0;
-      division = 0;
-      sumCondition = false;
-      multiplicationCondition = false;
-      divisionCondition = false;
-      substractionCondition = true;
-
-      break;
-
-    case `multiplication`:
-      if (substractionCondition === true) {
-        multiplication = mixCalc - parseFloat(arrayNumber.join(""));
-      } else if (sumCondition === true) {
-        multiplication = mixCalc + parseFloat(arrayNumber.join(""));
-      } else {
-        if (multiplicationCondition === true) {
-          multiplication = multiplication * parseFloat(arrayNumber.join(""));
+          sum = mixCalc - parseFloat(arrayNumber.join(""));
+        } else if (multiplicationCondition === true) {
+          sum = mixCalc * parseFloat(arrayNumber.join(""));
         } else if (divisionCondition === true) {
-          multiplication = mixCalc / parseFloat(arrayNumber.join(""));
+          sum = mixCalc / parseFloat(arrayNumber.join(""));
         } else {
-          (multiplication = parseFloat(arrayNumber.join("")))
-            ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
-            : (multiplication = calc);
+          if (sumCondition === true) {
+            sum = sum + parseFloat(arrayNumber.join(""));
+          } else {
+            (sum = parseFloat(arrayNumber.join("")))
+              ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+              : (sum = calc);
+          }
         }
-      }
 
-      mixCalc = multiplication;
-      arrayNumber = [];
-      initialValueDisplay();
+        mixCalc = sum;
+        arrayNumber = [];
+        initialValueDisplay();
 
-      substraction = 0;
-      sum = 0;
-      substractionCondition = false;
-      sumCondition = false;
-      multiplicationCondition = true;
+        substraction = 0;
+        multiplication = 0;
+        division = 0;
+        substractionCondition = false;
+        multiplicationCondition = false;
+        divisionCondition = false;
+        sumCondition = true;
 
-      break;
-
-    default:
-      if (substractionCondition === true) {
-        division = mixCalc - parseFloat(arrayNumber.join(""));
-      } else if (multiplicationCondition === true) {
-        division = mixCalc * parseFloat(arrayNumber.join(""));
-      } else if (sumCondition === true) {
-        division = mixCalc + parseFloat(arrayNumber.join(""));
-      } else {
-        if (divisionCondition === true) {
-          division = division / parseFloat(arrayNumber.join(""));
+        break;
+      case `resta`:
+        if (sumCondition === true) {
+          substraction = mixCalc + parseFloat(arrayNumber.join(""));
+        } else if (multiplicationCondition === true) {
+          substraction = mixCalc * parseFloat(arrayNumber.join(""));
+        } else if (divisionCondition === true) {
+          substraction = mixCalc / parseFloat(arrayNumber.join(""));
         } else {
-          (division = parseFloat(arrayNumber.join("")))
-            ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
-            : (division = calc);
+          if (substractionCondition === true) {
+            substraction = substraction - parseFloat(arrayNumber.join(""));
+          } else {
+            (substraction = parseFloat(arrayNumber.join("")))
+              ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+              : (substraction = calc);
+          }
         }
-      }
 
-      mixCalc = division;
-      arrayNumber = [];
-      initialValueDisplay();
+        mixCalc = substraction;
+        arrayNumber = [];
+        initialValueDisplay();
 
-      substraction = 0;
-      multiplication = 0;
-      sum = 0;
-      substractionCondition = false;
-      multiplicationCondition = false;
-      sumCondition = false;
-      divisionCondition = true;
-      break;
+        sum = 0;
+        multiplication = 0;
+        division = 0;
+        sumCondition = false;
+        multiplicationCondition = false;
+        divisionCondition = false;
+        substractionCondition = true;
+
+        break;
+
+      case `multiplication`:
+        if (substractionCondition === true) {
+          multiplication = mixCalc - parseFloat(arrayNumber.join(""));
+        } else if (sumCondition === true) {
+          multiplication = mixCalc + parseFloat(arrayNumber.join(""));
+        } else {
+          if (multiplicationCondition === true) {
+            multiplication = multiplication * parseFloat(arrayNumber.join(""));
+          } else if (divisionCondition === true) {
+            multiplication = mixCalc / parseFloat(arrayNumber.join(""));
+          } else {
+            (multiplication = parseFloat(arrayNumber.join("")))
+              ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+              : (multiplication = calc);
+          }
+        }
+
+        mixCalc = multiplication;
+        arrayNumber = [];
+        initialValueDisplay();
+
+        substraction = 0;
+        sum = 0;
+        substractionCondition = false;
+        sumCondition = false;
+        multiplicationCondition = true;
+
+        break;
+
+      default:
+        if (substractionCondition === true) {
+          division = mixCalc - parseFloat(arrayNumber.join(""));
+        } else if (multiplicationCondition === true) {
+          division = mixCalc * parseFloat(arrayNumber.join(""));
+        } else if (sumCondition === true) {
+          division = mixCalc + parseFloat(arrayNumber.join(""));
+        } else {
+          if (divisionCondition === true) {
+            division = division / parseFloat(arrayNumber.join(""));
+          } else {
+            (division = parseFloat(arrayNumber.join("")))
+              ? Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+              : (division = calc);
+          }
+        }
+
+        mixCalc = division;
+        arrayNumber = [];
+        initialValueDisplay();
+
+        substraction = 0;
+        multiplication = 0;
+        sum = 0;
+        substractionCondition = false;
+        multiplicationCondition = false;
+        sumCondition = false;
+        divisionCondition = true;
+        break;
+    }
+  } else {
+    alert("Debe resetear la caja registardora");
   }
 };
 
 const result = () => {
-  const display = document.getElementById("display");
-  if (sumCondition === true) {
-    calc = sum + parseFloat(arrayNumber.join(""));
-
-    display.innerText = `${calc.toFixed(2)} $`;
-    arrayNumber = [];
-    sum = 0;
-    mixCalc = 0;
-    sumCondition = false;
-  } else if (substractionCondition === true) {
-    calc = substraction - parseFloat(arrayNumber.join(""));
-
-    display.innerText = `${calc.toFixed(2)} $`;
-    arrayNumber = [];
-    substraction = 0;
-    mixCalc = 0;
-    substractionCondition = false;
-  } else if (multiplicationCondition === true) {
-    calc = multiplication * parseFloat(arrayNumber.join(""));
-
-    display.innerText = `${calc.toFixed(2)} $`;
-    arrayNumber = [];
-    multiplication = 0;
-    mixCalc = 0;
-    multiplicationCondition = false;
-  } else if (
-    multiplicationCondition === false &&
-    divisionCondition === false &&
-    sumCondition === false &&
-    substractionCondition === false
+  if (
+    display.innerText !== "MATH ERROR" &&
+    display.innerText !== "SYNTAX ERROR"
   ) {
-    if (Number.isNaN(parseFloat(arrayNumber.join(""))) === false) {
-      calc = parseFloat(arrayNumber.join(""));
-      display.innerText = `${calc.toFixed(2)} $`;
-    } else {
-      display.innerText = `${calc.toFixed(2)} $`;
-    }
-    arrayNumber = [];
-    sum = 0;
-    substraction = 0;
-    multiplication = 0;
-    division = 0;
-    mixCalc = 0;
-    sumCondition = false;
-    substractionCondition = false;
-    multiplicationCondition = false;
-    divisionCondition = false;
-  } else {
-    if (parseFloat(arrayNumber.join("")) !== 0) {
-      calc = division / parseFloat(arrayNumber.join(""));
+    const display = document.getElementById("display");
+    if (
+      sumCondition === true &&
+      Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+    ) {
+      calc = sum + parseFloat(arrayNumber.join(""));
 
       display.innerText = `${calc.toFixed(2)} $`;
       arrayNumber = [];
+      sum = 0;
+      mixCalc = 0;
+      sumCondition = false;
+    } else if (
+      substractionCondition === true &&
+      Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+    ) {
+      calc = substraction - parseFloat(arrayNumber.join(""));
+
+      display.innerText = `${calc.toFixed(2)} $`;
+      arrayNumber = [];
+      substraction = 0;
+      mixCalc = 0;
+      substractionCondition = false;
+    } else if (
+      multiplicationCondition === true &&
+      Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+    ) {
+      calc = multiplication * parseFloat(arrayNumber.join(""));
+
+      display.innerText = `${calc.toFixed(2)} $`;
+      arrayNumber = [];
+      multiplication = 0;
+      mixCalc = 0;
+      multiplicationCondition = false;
+    } else if (
+      multiplicationCondition === false &&
+      divisionCondition === false &&
+      sumCondition === false &&
+      substractionCondition === false
+    ) {
+      if (Number.isNaN(parseFloat(arrayNumber.join(""))) === false) {
+        calc = parseFloat(arrayNumber.join(""));
+        display.innerText = `${calc.toFixed(2)} $`;
+      } else {
+        display.innerText = `${calc.toFixed(2)} $`;
+      }
+      arrayNumber = [];
+      sum = 0;
+      substraction = 0;
+      multiplication = 0;
       division = 0;
       mixCalc = 0;
+      sumCondition = false;
+      substractionCondition = false;
+      multiplicationCondition = false;
       divisionCondition = false;
+    } else if (
+      divisionCondition === true &&
+      Number.isNaN(parseFloat(arrayNumber.join(""))) === false
+    ) {
+      if (parseFloat(arrayNumber.join("")) !== 0) {
+        calc = division / parseFloat(arrayNumber.join(""));
+
+        display.innerText = `${calc.toFixed(2)} $`;
+        arrayNumber = [];
+        division = 0;
+        mixCalc = 0;
+        divisionCondition = false;
+      } else {
+        display.innerText = "MATH ERROR";
+        calc = 0;
+
+        sum = 0;
+        substraction = 0;
+        multiplication = 0;
+        division = 0;
+        mixCalc = 0;
+        sumCondition = false;
+        substractionCondition = false;
+        multiplicationCondition = false;
+        divisionCondition = false;
+      }
     } else {
-      display.innerText = "MATH ERROR";
+      display.innerText = "SYNTAX ERROR";
+      calc = 0;
+
+      sum = 0;
+      substraction = 0;
+      multiplication = 0;
+      division = 0;
+      mixCalc = 0;
+      sumCondition = false;
+      substractionCondition = false;
+      multiplicationCondition = false;
+      divisionCondition = false;
     }
+  } else {
+    alert("Debe resetear la caja registardora");
   }
 };
 
-const deleteNumbers = () => {
+const reset = () => {
   arrayNumber = [];
   calc = 0;
 
